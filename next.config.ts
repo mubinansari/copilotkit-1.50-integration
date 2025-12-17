e96f1ec75cc2b32c10bdefb1e97f9b7f01986954
SonuMondal06 <sonu@memorang.com>
2025-12-17 13:15:14 +0530

    Update next.config.ts to include serverExternalPackages for build stability

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  // Required to prevent build failures
+  serverExternalPackages: ["pino", "pino-pretty", "thread-stream"],
 };
 
 export default nextConfig;
